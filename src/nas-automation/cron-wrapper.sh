f998c696f530a2b194238256f96edd8245107dc0
Emerson Martins Brito <emersonmrbr@gmail.com>
2025-09-01 09:42:10 -0300

    Add cron-wrapper.sh script

---
+++ src/nas-automation/cron-wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Directory where the scripts to be executed are located
+SCRIPT_DIR="/usr/etc"
+
+# List of scripts to be executed
+SCRIPTS=("${SCRIPT_DIR}/github-cron-wrapper.sh" "${SCRIPT_DIR}/paymos-cron-wrapper.sh")
+
+# Loop to execute each script in the list
+for script in "${SCRIPTS[@]}"; do
+    # Executes the script in the background
+    bash "$script" &
+    PROCESS=$!
+    # Waits for the background process to finish
+    wait $PROCESS
+    EXIT_CODE=$?
+    # Checks if there was an error during script execution
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo "Error executing $script (exit code: $EXIT_CODE)"
+        continue 1
+    fi
+    done
