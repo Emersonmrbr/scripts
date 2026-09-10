f3b721357e51dbcf5e44e3953803217c5efa1bfc
Emerson Martins Brito <emersonmrbr@gmail.com>
2026-09-10 01:21:27 -0300

    [change] Bay-pass paymo cron

--- src/nas-automation/cron-wrapper.sh
+++ src/nas-automation/cron-wrapper.sh
@@ -9,16 +9,18 @@ SCRIPTS=("${SCRIPT_DIR}"/*-cron-wrapper.sh)
 # Loop to execute each script in the list
 for script in "${SCRIPTS[@]}"; do
     # Executes the script in the background
+if [[ "$script" != *paymo* ]]; then
     bash "$script" < /dev/null &
     PROCESS=$!
     # Waits for the background process to finish
     wait $PROCESS
     EXIT_CODE=$?
     # Checks if there was an error during script execution
-    if [ $EXIT_CODE -ne 0 ]; then
+    if (( $EXIT_CODE != 0 )); then
         echo "Error executing $script (exit code: $EXIT_CODE)"
         continue
     fi
+fi
 done
 
 # Ensure all background processes are terminated
